7840f1b100f5203d08c440285782af9bb1842017
AatreyiDutt <aatreyi.dutt_ug23@ashoka.edu.in>
2022-05-05 15:50:22 +0530

    commented out speeds | legend attempt

--- scroll.js
+++ scroll.js
@@ -8,6 +8,36 @@ bearing: 0,
 pitch: 0
 });
 
+createLegend()
+
+function createLegend() {
+  //LEGEND TEXT
+  //the var layers array sets the text that will show up in the legend. you can enter any value here it is just text. Make sure that the legend values correspond to the ones you set in Mapbox.
+  var layers = ['Hindu & Sikh', 'Muslim'];
+
+  //LEGEND COLORS
+  //Set the corresponding LEGEND colors using HEX the easiest way to do this is by setting your mapcolors in Mapbox using ColorBrewer (colorbrewer2.org). Then copy the exact same hex value to the array below. Remember that each label above should correspond to a color. If the number of items in layers does not match the number of values in colors you will get an error.
+
+
+  var colors = ['#000000', '#ffffff'];
+
+//run through each element in the legend array and create a new legend item.
+  for (i = 0; i < layer.length; i++) {
+    var layered = layer[i];
+    var color = colors[i];
+    var item = document.createElement('div');
+    var key = document.createElement('span');
+    key.className = 'legend-key';
+    key.style.backgroundColor = color;
+
+    var value = document.createElement('span');
+    value.innerHTML = layer;
+    item.appendChild(key);
+    item.appendChild(value);
+    legend.appendChild(item);
+  }
+  //LEGEND CODE
+
 const chapters = {
 'part_1': {
 bearing:0,
@@ -20,21 +50,21 @@ duration: 7000,
 center: [70.5141829, 30.7656822],
 bearing: 315,
 zoom: 4.5,
-speed: 0.7,
+//speed: 0.7,
 pitch: 0
 },
 'part_3': {
 bearing: 0,
 center: [81.2383, 24.0767],
 zoom: 3.8,
-speed: 0.7,
+//speed: 0.7,
 pitch: 0
 },
 'part_4': {
 bearing: 45,
 center: [90.5736788, 23.6343083],
 zoom: 6,
-speed: 0.7,
+//speed: 0.7,
 pitch: 0,
 },
 'part_5': {
@@ -42,7 +72,7 @@ bearing: 45,
 center: [74.1835, 31.6239],
 zoom: 5,
 pitch: 10,
-speed: 0.7,
+//speed: 0.7,
 curve:1,
 pitch: 45
 },
@@ -50,7 +80,7 @@ pitch: 45
 bearing: 25,
 center: [80.9688997, 27.0272803],
 zoom: 6,
-speed: 0.7
+//speed: 0.7
 }
 };
 
